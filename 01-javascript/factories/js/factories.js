149e7ecf9ea266af21f5a2adfbcd93b50c5e6e31
wofockham <certainthings@gmail.com>
2022-03-11 15:43:20 +1100

    js factories and prototype examples

---
+++ 01-javascript/factories/js/factories.js
@@ -0,0 +1,54 @@
+// Factory (unofficial standard)
+
+const catFactory = function (n, a, f) {
+    return {
+        name: n,
+        age: a,
+        furColor: f,
+        carnivore: true,
+        species: 'feline',
+        meow: function() {
+            console.log('Meow, my name is', this.name);
+        }
+    };
+};
+
+
+const cuteCats = [
+    catFactory('Angel', 18, 'grey'),
+    catFactory('Evil', 14, 'red'),
+    catFactory('Meh', 12, 'white')
+];
+
+console.table(cuteCats);
+
+for (let i = 0; i < cuteCats.length; i++) {
+    const cat = cuteCats[i];
+    console.log(`${ cat.name } is ${ cat.furColor }`);
+}
+
+/////////////////////////////////////////////////////////////////////
+
+// Prototypes
+// Historically interesting but not essential
+// Especially now that we have classes
+// Probably don't use these.
+
+// Constructor (similar to a factory but with more magic)
+const Cat = function (n, a, f) {
+    // No need to create an object: an object is already there for you and it's called `this`.
+    this.name = n;
+    this.age = a;
+    this.furColor = f;
+    // No need to return anything, `this` gets returned automagically.
+};
+
+const cooper = new Cat('Cooper', 7, 'grey');
+const audrey = new Cat('Audrey', 7, 'grey');
+
+// The main "advantage"
+// You can extend the prototype AFTER you create your instances.
+
+console.log( audrey.species ); // undefined
+Cat.prototype.species = 'feline';
+console.log( audrey.species ); // 'feline'
