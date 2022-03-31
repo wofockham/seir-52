97c5d3470b8172112473a238fc21fefc118664b7
wofockham <certainthings@gmail.com>
2022-03-31 15:34:12 +1100

    sinatra butterflies

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -9,8 +9,37 @@ end
 # INDEX
 # Show all the butterflies (in summary)
 get '/butterflies' do
+    @butterflies = query_db 'SELECT * FROM butterflies'
+    erb :butterflies_index
+end
+
+# NEW
+get '/butterflies/new' do
+    erb :butterflies_new
+end
+
+# CREATE
+post '/butterflies' do
+    query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
+    redirect to('/butterflies') # GET
+end
+
+# SHOW
+# Show a single butterfly (in detail)
+get '/butterflies/:id' do
+    @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+    @butterfly = @butterfly.first # extract the butterfly from the array
+    erb :butterflies_show
+end
+
+
+
+
+def query_db(sql_statement)
+    puts sql_statement # Optional but nice for debugging
     db = SQLite3::Database.new 'database.sqlite3'
     db.results_as_hash = true
-    @butterflies = db.execute 'SELECT * FROM butterflies'
-    erb :butterflies_index
+    results = db.execute sql_statement
+    db.close
+    results
 end
