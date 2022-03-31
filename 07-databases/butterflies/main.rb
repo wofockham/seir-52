fff119bd71a62ff6e56573f1c807b84c18388fb0
wofockham <certainthings@gmail.com>
2022-03-31 13:00:33 +1100

    butterflies with sinatra

---
+++ 07-databases/butterflies/main.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+get '/' do
+    erb :home
+end
+
+# INDEX
+# Show all the butterflies (in summary)
+get '/butterflies' do
+    db = SQLite3::Database.new 'database.sqlite3'
+    db.results_as_hash = true
+    @butterflies = db.execute 'SELECT * FROM butterflies'
+    erb :butterflies_index
+end
