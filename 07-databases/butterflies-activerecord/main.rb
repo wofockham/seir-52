1564f0aa90c6d6c5bbea83c2f9b02b89eebfcaf8
wofockham <certainthings@gmail.com>
2022-04-01 16:14:23 +1100

    plants CRUD with activerecord

--- 07-databases/butterflies-activerecord/main.rb
+++ 07-databases/butterflies-activerecord/main.rb
@@ -12,7 +12,11 @@ ActiveRecord::Base.establish_connection(
 # Optional bonus:
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
+# Models ######################################################################
 class Butterfly < ActiveRecord::Base
+end
+
+class Plant < ActiveRecord::Base
 end
 
 get '/' do
@@ -74,4 +78,54 @@ get '/butterflies/:id/delete' do
     butterfly = Butterfly.find params[:id]
     butterfly.destroy
     redirect to('/butterflies') # choose your next victim
+end
+
+# Plants CRUD #################################################################
+
+# INDEX
+get '/plants' do
+    @plants = Plant.all
+    erb :plants_index
+end
+
+# NEW
+get '/plants/new' do
+    erb :plants_new
+end
+
+# CREATE
+post '/plants' do
+    plant = Plant.new
+    plant.name = params[:name]
+    plant.image = params[:image]
+    plant.save
+    redirect to("/plants/#{ plant.id }")
+end
+
+# SHOW
+get '/plants/:id' do
+    @plant = Plant.find params[:id]
+    erb :plants_show
+end
+
+# EDIT
+get '/plants/:id/edit' do
+    @plant = Plant.find params[:id]
+    erb :plants_edit
+end
+
+# UPDATE
+post '/plants/:id' do
+    plant = Plant.find params[:id]
+    plant.name = params[:name]
+    plant.image = params[:image]
+    plant.save
+    redirect to("/plants/#{ plant.id }")
+end
+
+# DELETE
+get '/plants/:id/delete' do
+    plant = Plant.find params[:id]
+    plant.destroy
+    redirect to('/plants')
 end
