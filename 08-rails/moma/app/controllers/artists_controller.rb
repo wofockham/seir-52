ff2761640cd3a08a367948b0beb3c9882564b971
wofockham <certainthings@gmail.com>
2022-04-06 13:03:00 +1000

    artists new/create

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -4,11 +4,23 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist
   end
 
   def edit
   end
 
   def show
+    @artist = Artist.find params[:id]
+  end
+
+  private # everything after this is not visible outside this file
+  def artist_params # strong params (see docs)
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
   end
 end
