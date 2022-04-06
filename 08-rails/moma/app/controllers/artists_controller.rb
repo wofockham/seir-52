168e67a9144d292c44bb3355ca5915d6021635ef
wofockham <certainthings@gmail.com>
2022-04-06 14:43:47 +1000

    moma artists crud done

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -13,13 +13,26 @@ class ArtistsController < ApplicationController
   end
 
   def edit
+    @artist = Artist.find params[:id]
+  end
+
+  def update
+    artist = Artist.find params[:id]
+    artist.update artist_params
+    redirect_to artist
   end
 
   def show
     @artist = Artist.find params[:id]
   end
 
-  private # everything after this is not visible outside this file
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path
+  end
+
+  private
   def artist_params # strong params (see docs)
     params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
   end
