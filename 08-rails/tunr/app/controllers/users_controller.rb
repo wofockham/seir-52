835ddd4f3497444a1243ff1461ab30abefcaf8df
wofockham <certainthings@gmail.com>
2022-04-07 17:37:08 +1000

    sign in

--- 08-rails/tunr/app/controllers/users_controller.rb
+++ 08-rails/tunr/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path # you can redirect wherever you want
     else
       render :new # show them the form again so they can try again
