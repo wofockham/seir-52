7ad30581c603bb4700169e9a29fa157773494306
wofockham <certainthings@gmail.com>
2022-04-20 12:33:04 +1000

    YOU LOVE TDD

---
+++ 09-tdd/bank/bank.rb
@@ -0,0 +1,24 @@
+class Bank
+    attr_reader :name, :accounts # getters
+
+    def initialize(n)
+        @name = n
+        @accounts = {}
+    end
+
+    def create_account(account_name, starting_deposit)
+        @accounts[account_name] = starting_deposit
+    end
+
+    def deposit(account_name, amount)
+        @accounts[account_name] += amount
+    end
+
+    def withdraw(account_name, amount)
+        @accounts[account_name] -= amount unless amount > @accounts[account_name]
+    end
+
+    def balance(account_name)
+        @accounts[account_name]
+    end
+end
