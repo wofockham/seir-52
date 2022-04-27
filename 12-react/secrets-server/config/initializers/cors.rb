95823fc6be85352ccd0fe69275789987459d42e2
wofockham <certainthings@gmail.com>
2022-04-27 14:17:18 +1000

    CORS enabled in Rails

---
+++ 12-react/secrets-server/config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# This config was jacked from rack-cors README on github (which see)
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins '*' # this needs to be an explicit list of URLs on Heroku
+        resource '*',
+            :headers => :any,
+            :methods => %i( get post put patch delete options head )
+    end
+end
