1615e29411c439b75bf9805cafdc8eacc1b33ce7
wofockham <certainthings@gmail.com>
2022-05-09 15:25:25 +1000

    regular expressions solutions

--- 14-advanced/regular-expressions/ex1.rb
+++ 14-advanced/regular-expressions/ex1.rb
@@ -1,6 +1,5 @@
-# lr.rb - A Ruby command line reader (equivalent to cat)
-# Usage: ruby lr.rb flintstones.txt
+# Make a program that prints each line of its input that mentions fred. (It shouldn’t do anything for other lines of input.) Does it match if your input string is Fred, frederick, or Alfred? Make a small text file with a few lines mentioning "fred flintstone" and his friends, then use that file as input to this program and the ones later in this section.
 
 ARGF.each do |line|
-    puts line if line =~ /Fred/
+    puts line if line =~ /fred/
 end
