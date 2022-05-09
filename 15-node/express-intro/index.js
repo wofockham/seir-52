d1b23bd847d20b29a9d9d494dfa11baefce77fef
wofockham <certainthings@gmail.com>
2022-05-09 15:52:17 +1000

    express intro part ii

--- 15-node/express-intro/index.js
+++ 15-node/express-intro/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const ejs = require('ejs'); // mustache / jade / handlebars
 const _ = require('underscore');
+const axios = require('axios');
 
 // Basic setup
 const server = express();
 server.set('view-engine', ejs);
 const PORT = 1337;
 
+// Views
 server.get('/', (req, res) => {
     const bros = 'Groucho Harpo Chico Zeppo'.split(' ');
     const randomBrother = _(bros).sample();
@@ -46,6 +48,22 @@ server.get('/info.json', (req, res) => {
         vice: 'cigars',
         luckyNumber: _.random(40)
     });
+});
+
+// Node equivalent to HTTParty to talk to APIs (axios)
+server.get('/trivia', (req, res) => {
+    axios.get('http://numbersapi.com/random/trivia').then((response) => {
+        res.send(response.data); // todo: this could be res.render('trivia.ejs');
+    });
+});
+
+// Form input/output
+server.get('/ask', (req, res) => {
+    res.render('ask.ejs');
+});
+
+server.get('/answer', (req, res) => {
+    res.render('answer.ejs', {question: req.query.question, answer: 'No comment'});
 });
 
 server.listen(PORT, () => console.log(`Now serving on http://localhost:${ PORT }`));
