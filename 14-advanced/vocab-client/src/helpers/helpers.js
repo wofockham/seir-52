4fe8cb62570520c6c2729d183a6a354d7d62098e
wofockham <certainthings@gmail.com>
2022-05-17 15:17:11 +1000

    words.vue

---
+++ 14-advanced/vocab-client/src/helpers/helpers.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3000/words/';
+
+// Higher order function to define error handling just once
+// You are not expected to understand this
+const handleError = (fn) => (...params) => 
+    fn(...params).catch((error) => {
+        console.log(error);
+    });
+
+export const api = {
+    getWord: handleError(async (id) => {
+        const res = await axios.get(baseURL + id);
+        return res.data;
+    }),
+    getWords: handleError(async () => {
+        const res = await axios.get(baseURL);
+        return res.data;
+    }),
+    deleteWord: handleError(async (id) => {
+        const res = await axios.delete(baseURL + id);
+        return res.data;
+    }),
+    createWord: handleError(async (payload) => {
+        const res = await axios.post(baseURL, payload);
+        return res.data;
+    }),
+    updateWord: handleError(async (payload) => {
+        const res = await axios.put(baseURL + payload._id, payload);
+        return res.data
+    })
+};
